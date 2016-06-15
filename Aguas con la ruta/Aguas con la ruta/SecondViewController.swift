bf9c02d4934cbdb702a6b3e83705c76c76a1a0dd
Josué Manuel Martínez Dávalos <josue_mtzd@icloud.com>
2016-06-14 21:51:56 -0500

    Primera versión
    Ahí vamos

---
+++ Aguas con la ruta/Aguas con la ruta/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Aguas con la ruta
+//
+//  Created by Josué Manuel Martínez Dávalos on 14/06/16.
+//  Copyright © 2016 Josué Manuel Martínez Dávalos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
